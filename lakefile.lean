1504ea7d7a455868a1ebcecf39369cb4a136bb33
cognivore <66186054+cognivore@users.66186054+cognivore@users.noreply.github.com.github.com>
2022-09-13 15:55:27 +0100

    Problem: Build broken: toolchain got bump elsewhere
    Solution:
    
     - Bump the commit hashes of the dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -4,13 +4,13 @@ open Lake DSL
 package Wasm
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "77fc51697abeff937ffd20d2050723dc0fa1c8c0"
+  "https://github.com/yatima-inc/LSpec.git" @ "c63610bb23451c7aa2faae17c71e8d162c6c616e"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "234ee564abf64c6e45f15ed921d94a3b4db228ab"
+  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "9c362443e0d89eb96683b52a1caaf762049697c4"
 
-require Straume from git
-  "https://github.com/yatima-inc/straume/" @ "d8b8084bee708a230379f53ff674570153a52f0d"
+require Megaparsec from git
+  "https://github.com/yatima-inc/Megaparsec.lean.git/" @ "395064f30d1723066250df97dc75a798a7770058"
 
 @[defaultTarget]
 lean_exe megaparsec {
