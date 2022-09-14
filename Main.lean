ed5aa98ddfe5e06fbcb74db7b5b27754e665be57
cognivore <jm@memorici.de>
2022-09-14 19:40:41 +0100

    Problem: There is no basic Wast exprs
    Solution:
    
    Based on the gist grammar: https://github.com/WebAssembly/spec/tree/master/interpreter#s-expression-syntax
    While reading the full grammar on demand: https://webassembly.github.io/spec/core/text/values.html
    Start implementing Wast.Expr. In this commit, add basic Wast exprs, namely:
    
     - Implement i32,64 and f32,64
     - Implement name and smart constructor for it
     - WIP: Name isn't opaque, so a user can make an invalid Name.

--- Main.lean
+++ Main.lean
@@ -1,4 +1,10 @@
 import Wasm
+import Wasm.Wast.Expr
+
+open  Wasm.Wast.Expr
+
+def sameName (_n₁ : Name x) (_n₂ : Name x) : Name "Слава Україні" := {}
+#eval sameName ({} : Name "lol") ({} : Name "lol")
 
 def main : IO Unit :=
   IO.println "(3) WASM demo coming soon."
