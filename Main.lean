ad949483f7017bfdbefb294123c0b7b39ccf8349
cognivore <66186054+cognivore@users.noreply.github.com>
2022-09-27 11:35:15 +0100

    Problem: No way for things that depend on x to depend on f x
    Solution: Write [wrong] Memo (see: https://zulip.yatima.io/#narrow/stream/20-meta/topic/WAST.20pair.20prog/near/24384)
    Make every field depend on Memo x ff (ff x) [should really be just Cached (ff x)].
    Note that we thread the dependencies through the fields of the structure, never "losing" dependency on x.
    
    Further work:
    
     - We currently use a bunch of `sorry`, andour theorems are likely wrong.
     - I hope that we can somehow prove that there's only one value for each Cached / Memo type.
       From this, we'll be able to prove that (\exists x : Cached \dots P(x)) \Rightleftarrow (\forall x : Cached \dots P(x)).
       Which means that if we have a proof of (\exists x : Cached \dots P(x)), we know that P(x) holds for `default : Cached \dots`.

--- Main.lean
+++ Main.lean
@@ -7,5 +7,8 @@ def sameName (_n₁ : Option $ Name x) (_n₂ : Option $ Name x) : Option (Name 
 #eval sameName (mkName "lol") (mkName "lol")
 -- #eval sameName (mkName "lol") (mkName "kek")
 
-def main : IO Unit :=
-  IO.println "(4) WASM demo coming soon."
+def main : IO Unit := do
+  IO.println "(5) WASM demo coming soon."
+  IO.println "Let's count the dots!"
+  let x : Nat'' "23" := {}
+  pure ()
