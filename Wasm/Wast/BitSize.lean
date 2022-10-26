3214635df8eb9537719f3c9652f4694251253698
cognivore <jm@memorici.de>
2022-10-26 20:54:56 +0100

    Problem: no support for SIMD packing in chunks of 128 bits
    Solution:
     - Add .hundredTwentyEight constructor
     - Add relevant instances

--- Wasm/Wast/BitSize.lean
+++ Wasm/Wast/BitSize.lean
@@ -3,6 +3,7 @@ We define BitSize inductive to then combine it with respective constructors. -/
 inductive BitSize :=
 | thirtyTwo
 | sixtyFour
+| hundredTwentyEight
 deriving BEq
 
 -- Boring instances
@@ -15,6 +16,10 @@ instance : OfNat BitSize 32 where
 instance : OfNat BitSize 64 where
   ofNat := .sixtyFour
 
+/- 128 *is* .hundredTwentyEight -/
+instance : OfNat BitSize 64 where
+  ofNat := .sixtyFour
+
 /- For something to depend on .thirtyTwo means that it can just as well depend on 32. -/
 instance : CoeDep BitSize BitSize.thirtyTwo Nat where
   coe := 32
@@ -23,11 +28,16 @@ instance : CoeDep BitSize BitSize.thirtyTwo Nat where
 instance : CoeDep BitSize BitSize.sixtyFour Nat where
   coe := 64
 
+/- For something to depend on .hundredTwentyEight means that it can just as well depend on 128. -/
+instance : CoeDep BitSize BitSize.hundredTwentyEight Nat where
+  coe := 64
+
 /- 32 *is* .thirtyTwo and 64 *is* .sixtyFour -/
 instance : Coe BitSize Nat where
   coe x := match x with
   | .thirtyTwo => 32
   | .sixtyFour => 64
+  | .hundredTwentyEight => 128
 
 /- We rely on numeric ordering rather than on derived ordering based on the order of constructors. -/
 instance : Ord BitSize where
