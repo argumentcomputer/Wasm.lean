e262f1ce84c30dfac97aa9d3960203ba93012bb0
mpenciak <matej.penciak@gmail.com>
2022-10-27 22:03:01 -0400

    bump toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -3,19 +3,19 @@ open Lake DSL
 
 package Wasm
 
-@[defaultTarget]
+@[default_target]
 lean_lib Wasm
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "c63610bb23451c7aa2faae17c71e8d162c6c616e"
+  "https://github.com/yatima-inc/LSpec.git" @ "02e423d02d2ba1b76bed3cf6459a5c2d7a13afb8"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "02ec0ac2415f2fffcb25638553c69113d28cd47c"
+  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "2b914196a8c67838e63c1c1e44eaf339b8a50eb7"
 
 require Megaparsec from git
-  "https://github.com/yatima-inc/Megaparsec.lean.git/" @ "8850513e4f9d18e7c61eb0023a085422874b9120"
+  "https://github.com/yatima-inc/Megaparsec.lean.git/" @ "e9238a79b3bef1be1bc1e99f26acc189bec12542"
 
-@[defaultTarget]
+@[default_target]
 lean_exe wasm {
   supportInterpreter := true
   root := "Main"
