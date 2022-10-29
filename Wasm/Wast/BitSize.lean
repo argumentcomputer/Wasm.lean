bbc8dddf71d51c36d867d257708d422216bfbf1b
cognivore <jm@memorici.de>
2022-10-29 20:24:28 +0100

    [WIP] Problem: No way to encode Modules
    [WIP] Solution:
    
     - Instead of going full wide, pick an end to end strategy of just compiling a small module to binary format and then all the way to Lurk
     - Add support for Func, but the Func body (OPS and INSTRUCTIONS) is still work in progrress.

--- Wasm/Wast/BitSize.lean
+++ Wasm/Wast/BitSize.lean
@@ -1,8 +1,24 @@
+import Megaparsec
+import Megaparsec.Char
+import Megaparsec.Common
+import Megaparsec.String
+import Megaparsec.Parsec
+
+open Megaparsec
+open Megaparsec.Char
+open Megaparsec.Common
+open Megaparsec.String
+open Megaparsec.Parsec
+
+
 /- Webassembly works on 32 and 64 bit ints and floats.
 We define BitSize inductive to then combine it with respective constructors. -/
 inductive BitSize :=
 | thirtyTwo
 | sixtyFour
+deriving BEq
+
+inductive BitSizeSIMD :=
 | hundredTwentyEight
 deriving BEq
 
@@ -17,8 +33,8 @@ instance : OfNat BitSize 64 where
   ofNat := .sixtyFour
 
 /- 128 *is* .hundredTwentyEight -/
-instance : OfNat BitSize 64 where
-  ofNat := .sixtyFour
+instance : OfNat BitSizeSIMD 128 where
+  ofNat := .hundredTwentyEight
 
 /- For something to depend on .thirtyTwo means that it can just as well depend on 32. -/
 instance : CoeDep BitSize BitSize.thirtyTwo Nat where
@@ -29,18 +45,29 @@ instance : CoeDep BitSize BitSize.sixtyFour Nat where
   coe := 64
 
 /- For something to depend on .hundredTwentyEight means that it can just as well depend on 128. -/
-instance : CoeDep BitSize BitSize.hundredTwentyEight Nat where
-  coe := 64
+instance : CoeDep BitSizeSIMD BitSizeSIMD.hundredTwentyEight Nat where
+  coe := 128
 
 /- 32 *is* .thirtyTwo and 64 *is* .sixtyFour -/
 instance : Coe BitSize Nat where
   coe x := match x with
   | .thirtyTwo => 32
   | .sixtyFour => 64
+
+/- 128 *is* .hundredTwentyEight. -/
+instance : Coe BitSizeSIMD Nat where
+  coe x := match x with
   | .hundredTwentyEight => 128
 
 /- We rely on numeric ordering rather than on derived ordering based on the order of constructors. -/
 instance : Ord BitSize where
   compare x y := Ord.compare (x : Nat) (y : Nat)
 
+/- We rely on numeric ordering rather than on derived ordering based on the order of constructors. -/
+instance : Ord BitSizeSIMD where
+  compare x y := Ord.compare (x : Nat) (y : Nat)
+
 -- End of boring instances
+
+def bitSizeP : Parsec Char String Unit BitSize :=
+  (string "32" *> pure BitSize.thirtyTwo) <|> (string "64" *> pure BitSize.sixtyFour)
--- Wasm/Wast/Code.lean
+++ Wasm/Wast/Code.lean
@@ -1,10 +1,97 @@
+import Megaparsec
+import Megaparsec.Common
+import Megaparsec.Errors.Bundle
+import Megaparsec.Parsec
+import Wasm.Wast.BitSize
+import Wasm.Wast.Name
+
+open Megaparsec
+open Megaparsec.Common
+open Megaparsec.Errors.Bundle
+open Megaparsec.Parsec
+open Wasm.Wast.Name
+
 namespace Wasm.Wast.Code
 
-namespace Module
-end Module
+namespace Type'
+
+inductive Type' where
+| f : BitSize → Type'
+| i : BitSize → Type'
+
+def typeP : Parsec Char String Unit Type' := do
+    let ps ← getParserState
+    let iorf ← (string "i" <|> string "f")
+    let bits ← bitSizeP
+    match iorf with
+    | "i" => pure $ Type'.i bits
+    | "f" => pure $ Type'.f bits
+    | _ => parseError $ .trivial ps.offset .none [] -- Impossible, but it's easier to write and more performant.
+
+end Type'
+
+namespace Local
+
+open Name
+open Type'
+
+structure Local where
+    name : (x : String) → Option $ Name x
+    type : Type'
+
+end Local
 
 namespace Func
+
+open Name
+open Type'
+
+structure Func where
+    name : (x : String) → Option $ Name x
+    export_ : Option String
+    params : List Local.Local
+    result : Option Type'
+    locals : List Local.Local
+
+
 end Func
+
+namespace Module
+
+/-
+
+(module $target
+    (func)
+    (func $f (export "(module (func))") (param $y f32) (param $y1 f32) (result f32)
+        (local $dummy i32)
+        i32.const 42
+        (local.set 2)
+        local.get $y1
+        (f32.add (local.get $y1))
+        local.get $y
+        f32.add
+    )
+    (func (export "main") (result f32)
+        (local $x f32) (local $y f32)
+        (local.set $x (f32.const 0.1))
+        (local.set $y (f32.const 20.95))
+        (call $f (local.get $x) (local.get $y))
+    )
+)
+
+-/
+
+open Name
+open Type'
+
+structure Module where
+    name : (x : String) → Option $ Name x
+    func : List Func.Func
+
+-- def moduleP : StateT Context (Parsec Char String Unit) Module := sorry
+def moduleP : Parsec Char String Unit Module := sorry
+
+end Module
 
 namespace Block
 end Block
