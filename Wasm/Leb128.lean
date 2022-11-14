90e8bbd8b5476b5e956393d08a376ff527e72e8d
cognivore <66186054+cognivore@users.noreply.github.com>
2022-11-14 13:15:52 +0000

    Problem: There's no way to emit supported binary wasm modules
    Solution:
    
    Part I. Scaffolding.
    
    0. Create Bytes.lean
    1. Construct \0asm magic and version
    2. Implement types section
    3. To check incremental implementation against reference implementation, use wasm-sandbox: https://github.com/cognivore/wasm-sandbox
    4. Implement result type encoding (!!! NB !!! currently we only support single-result functions !!! NB !!!)
    5. Implement function exports (!!! NB !!!! we only support those in `extractExports` for now.
    
    --- At this point we have extractTypes, extractFuncIds and extractExports implemented, so we can do the main part of the job now ---
    
    Part II. Mutually recursive AST and function body serialisation
    
    0. Refactor expr stuff in Code.lean in such way that it allows for recursive definitions.
      - It means that any operand may itself be an operation.
      - It means that our parsers are partial now.
      - It also means, since for the time being, we don't have a way to perform fully dependent parsing, that `Get` facility has to have a non-dependent version. Call it `Get'`.
      - We still use dependent Get in the parser for non-dependent Get', we just drop the dependency as we return (see get'PViaGet reads "get' parser via get").
      - Write a semi-automated test for it.
    1. Implement `lindex` function, which indexes binary blobs with their binary size "length-index" or something. Very useful.
    2. In order to implement iXX.add, implement and test LEB128 algorithm.
    3. Record a demo of producing wasm bytecode and then running it with `wasm-sandbox wevalf`, which evals a 0-ary function called "main" in wasm bytecode supplied via stdin.

---
+++ Wasm/Leb128.lean
@@ -0,0 +1,108 @@
+import YatimaStdLib
+
+namespace Wasm.Leb128
+
+def bitNot (x : Bit) : Bit :=
+  match x with
+  | .one => .zero
+  | .zero => .one
+
+def onesComplement (xs : List Bit) : List Bit :=
+  xs.map bitNot
+
+def summator (x y : Bit) : (Bit × Bit) :=
+  match (x, y) with
+  | (.one, .one) => (.one, .zero)
+  | (.zero, .one) => (.zero, .one)
+  | (.one, .zero) => (.zero, .one)
+  | (.zero, .zero) => (.zero, .zero)
+
+def plusOne (xs : List Bit) : List Bit :=
+  (xs.foldr (fun x acc =>
+    let res := summator x acc.1
+    (res.1, res.2 :: acc.2)
+    -- match (x, acc.1) with
+    -- (.one, .one)
+  ) (Bit.one, [])).2
+
+def twosComplement := plusOne ∘ onesComplement
+
+def nattob (x : Nat) (endianness : Endian := .big) : ByteArray :=
+  match endianness with
+  | .big => x.toByteArrayBE
+  | .little => x.toByteArrayLE
+
+def modPad (modulo : Nat) (bs : List Bit)
+           (padWith : Bit := .zero) (endianness : Endian := .big)
+           : List Bit := Id.run $ do
+  let l := bs.length
+  let rem := l % modulo
+  let to_replicate := if rem == 0 then
+    0
+  else
+    modulo - rem
+  let pad := List.replicate to_replicate padWith
+  dbg_trace s!"PADDING {bs} {pad}"
+  match endianness with
+  | .big => pad ++ bs
+  | .little => bs ++ pad
+
+def ntob (n : Nat) (endianness : Endian := .big) : ByteArray :=
+  match endianness with
+  | .big => n.toByteArrayBE
+  | .little => n.toByteArrayLE
+
+def pad7 (xs : List Bit) : List Bit := Id.run $ do
+  modPad 7 xs
+
+/- Remove all the leading zeroes -/
+def unlead (xs : List Bit) : List Bit :=
+  (xs.foldl (fun acc x =>
+    if acc.1 then
+      if x == Bit.zero then
+        (true, acc.2)
+      else
+        (false, acc.2 ++ [x])
+    else
+      (false, acc.2 ++ [x])
+  ) (true, [])).2
+
+def npad7 := pad7 ∘ unlead ∘ ByteArray.toBits ∘ ntob
+
+def sDisambiguatePosInt (xs : List Bit) : ByteArray :=
+  let go := (Nat.toByteArrayLE ∘ Bit.bitsToNat)
+  (match xs with
+  | Bit.zero :: Bit.one :: rest => (go (Bit.one :: Bit.one :: rest)) ++ ByteArray.mk #[0]
+  | _ => go xs)
+
+def spad7 (x : Int) : List Bit :=
+  let go := npad7 ∘ Int.natAbs
+  (if x >= 0 then
+    go
+  else
+    twosComplement ∘ go) x
+
+def reassemble (xs : List Bit) : List Bit := Id.run $ do
+  dbg_trace s!"{xs}"
+  (xs.foldl (fun acc x =>
+    dbg_trace s!"STEP ------------------ {acc} <<<<<<< {x}"
+    let leading_bit := if acc.1 then Bit.zero else Bit.one
+    let acc2' := if acc.2.1 == 6 then 0 else acc.2.1 + 1
+    let acc3' := if acc.2.1 == 0 then
+      acc.2.2 ++ [leading_bit, x]
+    else
+      acc.2.2 ++ [x]
+    (false, acc2', acc3')
+  ) (true, 0, [])).2.2
+
+def LebCore : List Bit → ByteArray :=
+  Nat.toByteArrayLE ∘ Bit.bitsToNat ∘ reassemble
+
+def uLeb128 : Nat → ByteArray :=
+  LebCore ∘ npad7
+
+def sLeb128 (x : Int) : ByteArray :=
+  (if x ≥ 0 then
+    sDisambiguatePosInt ∘ reassemble ∘ spad7
+  else
+    LebCore ∘ spad7) x
