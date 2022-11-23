ba712e028caaafb1d0c594c3a82815d268ad3491
cognivore <66186054+cognivore@users.noreply.github.com>
2022-11-23 03:56:26 +0000

    It works.

--- Wasm/Bytes.lean
+++ Wasm/Bytes.lean
@@ -43,6 +43,7 @@ def totalLength (bss : List ByteArray) : Nat :=
 def lindex (bss : ByteArray) : ByteArray :=
   uLeb128 bss.data.size ++ bss
 
+-- TODO: Refactor completely and support locals!
 def extractTypes (m : Module) : ByteArray :=
   let sigs := m.func.map $ fun x =>
     let params := x.params.map $ (b ∘ ttoi ∘ localToType)
@@ -108,13 +109,89 @@ def extractFuncs (fs : List Func) : ByteArray :=
   )
   header ++ (lindex $ fn ++ fbs)
 
+-- TODO
+def extractModName (_ : Module) := b0
+-- TODO
+def extractFuncNames (_ : List Func) := b0
+
+/-
+                       ___________________________________________________
+                      /                                                   \
+   _            _    |    _                                               |
+                     |            (_) __ _  ___| | ____ _| |              |
+                     |            | |/ _` |/ __| |/ / _` | |              |
+                     |            | | (_| | (__|   < (_| | |              |
+                     |           _/ |\__,_|\___|_|\_\__,_|_|              |
+                     |          |__/                                      |
+                     |                                                    |
+                     |                         _   _  __ _          _     |
+                     |            ___ ___ _ __| |_(_)/ _(_) ___  __| |    |
+                     |           / __/ _ \ '__| __| | |_| |/ _ \/ _` |    |
+                     |          | (_|  __/ |  | |_| |  _| |  __/ (_| |    |
+                     |           \___\___|_|   \__|_|_| |_|\___|\__,_|    |
+                     |                                                    |
+        _            ,_     ______________________________________________.
+       / \      _-'    |   /
+     _/|  \-''- _ /    |  /
+__-' { |          \    | /
+    /             \    |/
+    /       "o.  |o }
+    |            \ ;                 TODO: generalise
+                  ',                    wasm maps
+       \_         __\               and indirect maps
+         ''-_    \.//
+           / '-____'
+          /
+        _'
+      _-
+
+-/
+def indexNamedLocals (f : Func) : List (Nat × LocalName) :=
+  let go (acc : (Nat × List (Nat × LocalName))) (x : Local) :=
+    let i := acc.1
+    match x with
+    | .name y => (i + 1, (i, y) :: acc.2)
+    | _ => (i + 1, acc.2)
+  let indexed_params := f.params.foldl go (0, [])
+  let params := indexed_params.2.reverse
+  let locals_proper := (f.locals.foldl go (indexed_params.1, [])).2.reverse
+  params ++ locals_proper
+
+def indexFunctionsWithNamedLocals (fs : List Func) : List (Nat × List (Nat × LocalName)) :=
+  let go (acc : (Nat × List (Nat × List (Nat × LocalName)))) (x : Func) :=
+    let i := acc.1
+    let ls := indexNamedLocals x
+    if ls.length > 0 then
+      (i + 1, (i, ls) :: acc.2)
+    else
+      (i + 1, acc.2)
+  (fs.foldl go (0, [])).2.reverse
+
+def encodeLocal (l : Nat × LocalName) : ByteArray :=
+  uLeb128 l.1 ++ uLeb128 l.2.name.length ++ l.2.name.toUTF8
+
+def encodeFunc (f : (Nat × List (Nat × LocalName))) : ByteArray :=
+  let locals := f.2.map encodeLocal
+  uLeb128 f.1 ++ uLeb128 f.2.length ++ flatten locals
+
+def extractLocalNames (fs : List Func) : ByteArray :=
+  let subsection_header := b 0x02
+  let ifs := (indexFunctionsWithNamedLocals fs).map encodeFunc
+  subsection_header ++ (lindex $ uLeb128 ifs.length ++ flatten ifs)
+
 def extractNames (m : Module) : ByteArray :=
-  sorry
+  let header := b 0x00
+  -- let name := ByteArray.mk #[0x6e, 0x61, 0x6d, 0x65] -- == literal "name"
+  let name := "name".toUTF8
+  let modName := extractModName m
+  let funcNames := extractFuncNames m.func
+  let locNames := extractLocalNames m.func
+  header ++ (lindex $ (lindex name) ++ modName ++ funcNames ++ locNames)
 
 def mtob (m : Module) : ByteArray :=
   magic ++
   version ++
   (extractTypes m) ++
   (extractFuncIds m) ++
-  (extractFuncs m.func) -- ++
-  -- (extractNames m)
+  (extractFuncs m.func) ++
+  (extractNames m)
