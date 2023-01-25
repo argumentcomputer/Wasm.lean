eb300bc2f3aa6b5d9864c00d846d54bdfa75493b
cognivore <66186054+cognivore@users.noreply.github.com>
2023-01-25 14:17:32 +0000

    Hypothesis: Builds with new Megaparsec and no Yatima dep
    Outcome: Negative https://asciinema.org/a/qiVe9u7AUWOwfoMWA9y9M7CGG
    
    Experiment:
    
     - Bump Mega, make sure everything else builds and works
     - Bump YSL
     - Remove unneeded code from the test
     - Run tests (works somehow)
     - Build the project (fails confusingly, see asciinema)

--- Tests/Dependent.lean
+++ Tests/Dependent.lean
@@ -26,68 +26,6 @@ def isNone (x : Option α) : Bool :=
   | some _ => false
   | none => true
 
--- Aho–Corasick string search algorithm.
--- This is a very fast way to search for a substring in a string.
--- It is used to find the first occurence of a substring in a string.
--- We use it to find the first occurence of an underscore in a string.
-partial def findFirst (s : String) (sub : String) : Option String.Pos := do
-  let subLen := sub.length
-  let sLen := s.length
-  let pospp x := String.Pos.mk $ x.byteIdx + 1
-  let mut i := String.Pos.mk 0
-  let mut j := 0
-  let mut k := 0
-  let mut lps := [0]
-  while j < String.Pos.mk subLen do
-    -- Debug steps
-    dbg_trace s!"j: {j.byteIdx} k: {k.byteIdx} lps: {lps}"
-    if sub.get! j == sub.get! k then
-      k := pospp k
-      -- TODO: use Array instead of this.
-      lps := lps ++ [k]
-      j := pospp j
-    else
-      if k != 0 then
-        k := lps.get! k.byteIdx
-      else
-        lps := lps ++ [0]
-        j := pospp j
-  j := 0
-  let mut result : Option String.Pos := .none
-  while i.byteIdx < sLen do
-    -- Debug steps
-    dbg_trace s!"i: {i.byteIdx} j: {j.byteIdx}"
-    if sub.get! j == s.get! i then
-      j := pospp j
-      i := pospp i
-    if j == String.Pos.mk subLen then
-      result := .some (i - j)
-    else if i < String.Pos.mk sLen && sub.get! j != s.get! i then
-      if j != 0 then
-        j := lps.get! j.byteIdx
-      else
-        i := pospp i
-  result
-
--- Test findFirst function on a variety of strings and substrings.
-def testFindFirst : TestSeq := Id.run $ do
-  -- Group tests together.
-  group "findFirst:" $
-    -- First test that findFirst returns .none when the substring is not found.
-    test " - returns none when substring is not found" (isNone $ findFirst "hello" "world") $
-    -- Now test that findFirst returns .some when the substring is found.
-    test " - returns some when substring is found" (isSome $ findFirst "hello" "ll") $
-    -- Now test that findFirst returns the correct position when the substring is found.
-    test " - returns correct position when substring is found" $ (findFirst "hello" "ll").get! == String.Pos.mk 2
-
-
--- A function that replaces a substirng in a string.
--- We use splitAtString to split the string into two parts, then we join them back together with the replacement string.
--- def replace (s : String) (old : String) (new : String) : String :=
---   let (before, after) := splitAtString s old
---   s!"{before}{new}{after}"
-
-
 -- Happy path tests. We check that Nats and Floats are parsed.
 def testNat : TestSeq := Id.run $ do
   -- Convert these declarations into a list of strings please.
@@ -116,8 +54,8 @@ def twoPlusTwo : TestSeq :=
 
 -- Run all the TestSeq defined in this module.
 def main : IO UInt32 := do
-  dbg_trace "Running tests..."
   lspecIO $
-    twoPlusTwo
-    -- testNat -- ++
+    test "four equals four" (4 = 4) ++
+    twoPlusTwo ++
+    testNat -- ++
     -- testFindFirst
--- lakefile.lean
+++ lakefile.lean
@@ -12,13 +12,10 @@ require LSpec from git
   "https://github.com/yatima-inc/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "649368d593f292227ab39b9fd08f6a448770dca8"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "49ee890897dbdd4665d0e8c75cd3401f0b4e6f21"
 
 require Megaparsec from git
-  "https://github.com/yatima-inc/Megaparsec.lean" @ "2e7eb85008f8bfb9d246ae0508e167e7a0de2eb3"
-
-require Yatima from git
-  "https://github.com/yatima-inc/yatima-lang" @ "517ec2f8fa7c5bcd0bccafc96c28eb81a8f1dbf0"
+  "https://github.com/yatima-inc/Megaparsec.lean" @ "85ffa8b5e9bd0bc9b9020fd4e0fe1baa89d69413"
 
 @[default_target]
 lean_exe wasm {
