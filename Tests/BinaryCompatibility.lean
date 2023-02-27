dcd813d4e363c974674301dde17ee69bca61fe57
Ilona Prikule <ilona@yatima.io>
2023-02-27 22:44:06 +0100

    Rehaul framework for binary compatibility tests
    Problem: though what we have works, it has two issues:
    1. it doesn't use LSpec awesome capabilities, resulting in a lot of
       boilerplate code that is difficult to extend;
    2. more importantly, it silently fails on wasm-sandbox and parse errors.
    
    Solution: rewrote it to use more of LSpec API, incl. to report errors.
    
    Closes #39.

--- Tests/BinaryCompatibility.lean
+++ Tests/BinaryCompatibility.lean
@@ -8,45 +8,16 @@ open Megaparsec.Parsec
 open Wasm.Wast.Parser
 open Wasm.Bytes
 
--- Success and failure
-structure Success where
-  stdout : String
-  stderr : String
+inductive ExternalFailure (e : IO.Error) : Prop
+instance : Testable (ExternalFailure e) := .isFailure s!"External failure: {e}"
 
-structure Failure where
-  stdout : String
-  stderr : String
-  exitCode : UInt32
+open Megaparsec.Errors.Bundle in
+inductive ParseFailure (src : String) (e : ParseErrorBundle Char String Unit) : Prop
+instance : Testable (ParseFailure src e) := .isFailure s!"Parsing:\n{src}\n{e}"
 
-structure Fault where
-  error : IO.Error
-
-def run (args : IO.Process.SpawnArgs)
-  : IO $ Except (Except Fault Failure) Success := do
-  match (← IO.Process.output args |>.toBaseIO) with
-  | .ok out =>
-    if out.exitCode = 0 then
-      pure $ .ok $ Success.mk out.stdout out.stderr
-    else
-      pure $ .error $ .ok $ Failure.mk out.stdout out.stderr out.exitCode
-  | .error err => pure $ .error $ .error $ Fault.mk err
-
-def doesWasmSandboxRun? := do
-  run { cmd := "./wasm-sandbox", args := #["wast2bytes", "(module)"] }
-
-def w2b (x : String) := do
-  run { cmd := "./wasm-sandbox", args := #["wast2bytes", x] }
-
-def testCanary : TestSeq :=
-  test "This is a canary test. Chirp chirp." true
-
-def testCrow : TestSeq :=
-  -- test "This is a crow test. Caw caw." false
-  test
-    "You need to have `wasm-sandbox` binary in your current work directory.
-    Please run:
-    wget https://github.com/cognivore/wasm-sandbox/releases/download/v1/wasm-sandbox && chmod +x ./wasm-sandbox"
-    false
+open IO.Process (run) in
+def w2b (x : String) :=
+  run { cmd := "./wasm-sandbox", args := #["wast2bytes", x] } |>.toBaseIO
 
 -- A very bad hasing function. It adds up the character codes of each of the string's characters, and then appends 'L' followed by the number of characters in the string to reduce the chance of collisions.
 def myHash (s : String) : String :=
@@ -55,6 +26,56 @@ def myHash (s : String) : String :=
 
 def fname (s : String) : String :=
   "./wast-dump-" ++ myHash s ++ ".bytes"
+
+/- Generic Wasm module test. -/
+partial def testGeneric : String → ByteArray → ByteArray → TestSeq
+  | mod, rs, os =>
+    let str := s!"Binary representation is compatible with {fname mod}
+    === CODE ===
+    {mod}
+    ||| END OF CODE |||"
+    test str $ rs = os
+
+/- Here's how Main used to test a particular module:
+
+  IO.println s!"{i} is represented as:"
+  let o_parsed_module ← parseTestP moduleP i
+  match o_parsed_module.2 with
+  | .error _ => IO.println "FAIL"
+  | .ok parsed_module => do
+    IO.println s!">>> !!! >>> It is converted to bytes as: {mtob parsed_module}"
+    let f := System.mkFilePath ["/tmp", "mtob.41.wasm"]
+    let h ← IO.FS.Handle.mk f IO.FS.Mode.write
+    h.write $ mtob parsed_module
+    gO.println "It's recorded to disk at /tmp/mtob.41.wasm"
+
+Instead of this, here, we're going to:
+
+ 1. Make the reference WASM bytes with `wasm-sandbox` invocation,
+    as provided by function `w2b`.
+ 2. Read the reference bytes into a variable.
+ 3. Compile our bytes and read those into a variable.
+ 4. Write our bytes into a file called s!"{fname}.lean".
+ 5. Byte by byte, compare the two variables, reporting errors.
+-/
+open IO.FS in
+partial def moduleTestSeq (x : String) : IO TestSeq := do
+  -- Run w2b against x
+  match ←w2b x with
+  | .ok _ => do
+    -- Read the reference bytes into a variable.
+    let ref_bytes ← readBinFile $ fname x
+    -- Compile our bytes and read those into a variable.
+    match parse moduleP x with
+    | .error pe =>
+      pure $ test "parsing module" (ParseFailure x pe)
+    | .ok module => do
+      let our_bytes := mtob module
+      -- Write our bytes into a file called s!"{fname}.lean".
+      writeBinFile s!"{fname x}.lean" our_bytes
+      pure $ testGeneric x ref_bytes our_bytes
+  | .error e =>
+    pure $ test "failed to encode with wasm-sandbox" (ExternalFailure e)
 
 def uWasmMods := [
   "(module
@@ -128,71 +149,21 @@ def uWasmMods := [
     ))"
 ]
 
-/- Generic Wasm module test that uses loop to check for stuff. -/
-partial def testGeneric : String → ByteArray → ByteArray → TestSeq
-  | mod, rs, os =>
-    let str := s!"Binary representation is compatible with {fname mod}
-    === CODE ===
-    {mod}
-    ||| END OF CODE |||"
-    test str $ rs = os
-
-/- Here's how Main used to test a particular module:
-
-  IO.println s!"{i} is represented as:"
-  let o_parsed_module ← parseTestP moduleP i
-  match o_parsed_module.2 with
-  | .error _ => IO.println "FAIL"
-  | .ok parsed_module => do
-    IO.println s!">>> !!! >>> It is converted to bytes as: {mtob parsed_module}"
-    let f := System.mkFilePath ["/tmp", "mtob.41.wasm"]
-    let h ← IO.FS.Handle.mk f IO.FS.Mode.write
-    h.write $ mtob parsed_module
-    gO.println "It's recorded to disk at /tmp/mtob.41.wasm"
-
-Instead of this, here, we're going to:
-
- 1. Make the reference WASM bytes with `wasm-sandbox` invocation,
-    as provided by function `w2b`.
- 2. Read the reference bytes into a variable.
- 3. Compile our bytes and read those into a variable.
- 4. Write our bytes into a file called s!"{fname}.lean".
- 5. Byte by byte, compare the two variables, reporting errors.
--/
-partial def moduleTestIO (x : String) : IO UInt32 := do
-  -- Run w2b against x
-  -- If we failed for any reason, we return false, otherwise, we go ahead to the next step, encoded as another function.
-  match (← w2b x) with
-  | .error _ =>
-    pure 35
-  | .ok _w2b_x => do
-    -- Read the reference bytes into a variable.
-    let ref_bytes ← IO.FS.Handle.mk (fname x) IO.FS.Mode.read >>=
-      fun h => h.readBinToEnd
-    -- Compile our bytes and read those into a variable.
-    match parseP moduleP "" x with
-    | .error _ =>
-      pure 55
-    | .ok parsed_module => do
-      let our_bytes := mtob parsed_module
-      -- Write our bytes into a file called s!"{fname}.lean".
-      let h ← IO.FS.Handle.mk s!"{fname x}.lean" IO.FS.Mode.write
-      h.write our_bytes
-      lspecIO $ testGeneric x ref_bytes our_bytes
-
-def preservingErrorCode (acc : UInt32) (x : IO UInt32) : IO UInt32 := do
-  match (← x) with
-  | 0 => pure acc
-  | n => pure n
+open IO.Process (run) in
+def doesWasmSandboxRun? :=
+  run { cmd := "./wasm-sandbox", args := #["wast2bytes", "(module)"] } |>.toBaseIO
 
 def withWasmSandboxRun : IO UInt32 :=
-  -- For each element in uWasmMods, run moduleTestIO on it.
-  uWasmMods.foldlM (fun acc x => preservingErrorCode acc $ moduleTestIO x) 0
+  let testCases := [ uWasmMods ]
+  lspecEachIO testCases.join moduleTestSeq
 
 def withWasmSandboxFail : IO UInt32 :=
-  lspecIO $ testCrow
+  lspecIO $ test "wasm-sandbox check"
+    (ExternalFailure "You need to have `wasm-sandbox` binary in your current work directory.
+    Please run:
+    wget https://github.com/cognivore/wasm-sandbox/releases/download/v1/wasm-sandbox && chmod +x ./wasm-sandbox")
 
 def main : IO UInt32 := do
   match (← doesWasmSandboxRun?) with
-  | Except.ok _ => withWasmSandboxRun
+  | .ok _ => withWasmSandboxRun
   | _ => withWasmSandboxFail
