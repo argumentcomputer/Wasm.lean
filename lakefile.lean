2c290bc696c33febb2f9f52b52c84155b57ba671
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-24 11:40:00 -0300

    update deps org (#56)

--- lakefile.lean
+++ lakefile.lean
@@ -9,16 +9,16 @@ package Wasm {
 lean_lib Wasm
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+  "https://github.com/lurk-lab/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "9a49b49fc35a960ee333b47c4960390543defabd"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "9a49b49fc35a960ee333b47c4960390543defabd"
 
 require Megaparsec from git
-  "https://github.com/yatima-inc/Megaparsec.lean" @ "8859f129d199d5be82962140e9b886c0fd49e89c"
+  "https://github.com/lurk-lab/Megaparsec.lean" @ "8859f129d199d5be82962140e9b886c0fd49e89c"
 
 -- require Yatima from git
---   "https://github.com/yatima-inc/yatima-lang" @ "26e896debf14cf3bb09a7a8a00f70583ae95469d"
+--   "https://github.com/lurk-lab/yatima" @ "26e896debf14cf3bb09a7a8a00f70583ae95469d"
 
 @[default_target]
 lean_exe wasm {
